04781955ccb130f5afc11c897bb05a778f85d51b
Joshua Honeycutt <joshua.honeycutt@gmail.com>
2017-10-10 15:01:48 -0500

    Remove old stuff Push copy of form recognition with movement

--- pob-sdk/dualpob/sources/Firmware_POBEYE/readme.txt
+++
@@ -1,8 +0,0 @@
-
-This repertory contains the source code of the pobeye firmware.
-
-
-How to Use
-==========
-
-Use this firmware only if you want manage the pobeye from the dualpob.
--- pob-sdk/pobeye-java/examples/pob-bot-demo/Pattern.java
+++
@@ -1,9 +0,0 @@
-/* This file is machine generated ! do not edit ! */
-
-class Pattern
-{
-	public static final int CROSS_BMP = 1;
-	public static final int TOWER_BMP = 2;
-	public static final int TRIANGLE_BMP = 3;
-
-}
